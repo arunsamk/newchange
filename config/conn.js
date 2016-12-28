88e584f81a5224cb7d8b126eca33be9b7f5d132e
ArunKSamson <arunsamk@gmail.com>
2016-12-28 16:10:00 +0530

    28.12.2016.004

--- config/conn.js
+++ config/conn.js
@@ -1,5 +1,5 @@
 //Connection Statement
 module.exports = {
 	//url: 'mongodb://localhost/churchfinder'
-    url: 'mongodb://Princy:genius@ec2-35-154-93-143.ap-south-1.compute.amazonaws.com:27017/churchlocator'
+    url: 'mongodb://admin:admin@ec2-35-154-93-143.ap-south-1.compute.amazonaws.com:27017/churchlocator'
 };
