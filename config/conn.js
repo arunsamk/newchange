ed006602af745c0feda5c9b3f602be9d5bde6d7c
ArunKSamson <arunsamk@gmail.com>
2016-12-28 13:04:56 +0530

    28.12.1992.001

--- config/conn.js
+++ config/conn.js
@@ -1,5 +1,5 @@
 //Connection Statement
 module.exports = {
-	url: 'mongodb://localhost/churchfinder'
-    //url: 'mongodb://Princy:genius@ec2-35-154-93-143.ap-south-1.compute.amazonaws.com:27017/dummyDB'
+	//url: 'mongodb://localhost/churchfinder'
+    url: 'mongodb://Princy:genius@ec2-35-154-93-143.ap-south-1.compute.amazonaws.com:27017/dummyDB'
 };
