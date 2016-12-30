4ffe7c5c69315b32242c8e887695d93fa9b31358
princyabi <princyabisheka@gmail.com>
2016-12-30 13:03:10 +0530

    30.12.2016.003

--- config/conn.js
+++ config/conn.js
@@ -1,5 +1,5 @@
 //Connection Statement
 module.exports = {
 	//url: 'mongodb://localhost/churchfinder'
-    url: 'mongodb://ec2-35-154-100-62.ap-south-1.compute.amazonaws.com'
+    url: 'mongodb://ec2-35-154-100-62.ap-south-1.compute.amazonaws.com:27017'
 };
